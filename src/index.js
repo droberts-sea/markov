68ec7da63adc62edc7b440ed6dbe3d6831348611
Dan Roberts <dan@adadevelopersacademy.org>
2019-05-10 22:24:17 -0700

    Start working on the text ingestion pipeline

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-module.exports = function markov(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  return string.replace(/\s/g, "");
+const fs = require('fs');
+
+const ingest = (filename) => {
+  console.log(`Reading file ${filename}...`);
+  
+  fs.readFile(filename, (err, data) => {
+    if (err) throw err;
+    console.log(`Read ${data.length} characters.`);
+    
+    process(data);
+  });
 };
+
+const process = (data) => {
+  console.log(`data is of class ${data.constructor.name}`);
+  data = data.toString();
+  
+  const lines = data.split('\n');
+  lines.forEach(line => {
+    if (line === '') {
+      console.log("Empty line");
+    } else {
+      const tokens = line.split(' ');
+      console.log(`Line beginning with ${tokens[0]} and containing ${tokens.length} tokens`);
+    }
+  });
+}
+
+const FILENAME = 'data/wwii.txt';
+ingest(FILENAME);
+
+
+// module.exports = function markov(string) {
+//   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+//   return string.replace(/\s/g, "");
+// };

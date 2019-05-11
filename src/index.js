40efed50e18ad2244646545655145faf523544ee
Dan Roberts <dan@adadevelopersacademy.org>
2019-05-10 22:30:05 -0700

    Convert loose functions to a class

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
 const fs = require('fs');
 
-const ingest = (filename) => {
-  console.log(`Reading file ${filename}...`);
-  
-  fs.readFile(filename, (err, data) => {
-    if (err) throw err;
-    console.log(`Read ${data.length} characters.`);
-    
-    process(data);
-  });
-};
+class Markov {
 
-const process = (data) => {
-  console.log(`data is of class ${data.constructor.name}`);
-  data = data.toString();
-  
-  const lines = data.split('\n');
-  lines.forEach(line => {
-    if (line === '') {
-      console.log("Empty line");
-    } else {
-      const tokens = line.split(' ');
-      console.log(`Line beginning with ${tokens[0]} and containing ${tokens.length} tokens`);
-    }
-  });
+  constructor() {
+    console.log("In the constructor");
+    
+  }
+
+  ingest(filename) {
+    console.log(`Reading file ${filename}...`);
+    
+    fs.readFile(filename, (err, data) => {
+      if (err) throw err;
+      console.log(`Read ${data.length} characters.`);
+      
+      this.process(data);
+    });
+  }
+
+  process(data) {
+    console.log(`data is of class ${data.constructor.name}`);
+    data = data.toString();
+    
+    const lines = data.split('\n');
+    lines.forEach(line => {
+      if (line === '') {
+        console.log("Empty line");
+      } else {
+        const tokens = line.split(' ');
+        console.log(`Line beginning with ${tokens[0]} and containing ${tokens.length} tokens`);
+      }
+    });
+  }
 }
 
 const FILENAME = 'data/wwii.txt';
-ingest(FILENAME);
+const markov = new Markov();
+markov.ingest(FILENAME);
 
 
 // module.exports = function markov(string) {
